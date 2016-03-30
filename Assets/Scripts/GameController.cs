1d2360e6374e2416a86c53b22dea2116f2a9a8c1
Aldrin Caberio <aldrin_caberio@yahoo.com>
2016-03-31 00:44:07 +0800

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,333 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	[Serializable]
+	public class PlatformSettings
+	{
+		public GameObject[] platforms;
+
+		public float spawn_x;
+		public float spawn_y;
+		public float spawn_start_wait;
+		public float spawn_min_wait;
+		public float spawn_max_wait;
+
+		public float scroll_speed;
+		public float fall_delay = 1f;
+		public float fall_speed = 1f;
+	}
+
+	[Serializable]
+	public class FallingSettings
+	{
+		public GameObject[] fallings;
+
+		public float spawn_x;
+		public float spawn_y;
+		public int max_spawn;
+		public float spawn_rate;
+		public float spawn_wait;
+		public float spawn_start_wait;
+
+		public float fall_speed;
+	}
+
+	[Serializable]
+	public class LavaSettings
+	{
+		public float scroll_speed;
+		public float scroll_range;
+
+		public int damage;
+	}
+
+	[Serializable]
+	public class CloundSettings
+	{
+		public float scroll_speed;
+		public float scroll_range;
+
+		public int damage;
+	}
+
+	[Serializable]
+	public class PlayerSettings
+	{
+		public GameObject player;
+		public int hp = 100;
+		public float move_force = 365f;
+		public float max_speed = 5f;
+		public float jump_force = 1000f;
+		public float scale_rate = 0.25f;
+	}
+
+	//settings
+	public PlayerSettings player_settings;
+	public PlatformSettings platform_settings;
+	public FallingSettings falling_settings;
+	public LavaSettings lava_settings;
+	public float scroll_speed;
+
+	public static GameController instance = null;
+
+	//privates
+	private bool play_game;
+	private bool game_start;
+	private bool game_over;
+
+	private Transform platforms_holder;
+	private Transform fallings_holder;
+
+	private ParticleSystem erruption;
+	private ParticleSystem ashes;
+	private ParticleSystem clounds;
+
+	private GameObject blockImage;
+	private GameObject game_over_image;
+
+	private Text message_text;
+
+	private GameObject map;
+
+	[HideInInspector]
+	public int score;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+			    
+		DontDestroyOnLoad(gameObject);
+
+		ShowGameMap (false);
+		ShowMainMenuUI (true);
+	}
+		
+	void Start () {
+		play_game = false;
+		score = player_settings.hp;
+	}
+
+	void OnLevelWasLoaded(int level) {
+		score = player_settings.hp;
+
+		ShowMainMenuUI (false);
+		ShowIntroductionUI (true);
+		ShowInGameUI (true);
+		ShowGameMap (true);
+		InitGame ();
+	}
+
+	public void InitGame(){
+		GetReference ();
+
+		game_start = false;
+		game_over = false;
+
+		message_text.text = "Welcome To The Olympus!\n\n" +
+		"The world was ruined by the violent volcano.\n" +
+		"Please help the rock hero - Osamus \nto escape from this chaotic place.";
+
+		GameObject toInstantiate = platform_settings.platforms [0];
+		(Instantiate (toInstantiate, new Vector3(-6.5f, -7.5f, 0), Quaternion.identity) as GameObject).gameObject.transform.SetParent(platforms_holder.transform);
+		(Instantiate (toInstantiate, new Vector3(0f, -7.5f, 0), Quaternion.identity) as GameObject).gameObject.transform.SetParent(platforms_holder.transform);
+		(Instantiate (toInstantiate, new Vector3(6.5f, -7.5f, 0), Quaternion.identity) as GameObject).gameObject.transform.SetParent(platforms_holder.transform);
+		(Instantiate (toInstantiate, new Vector3(13f, -7.5f, 0), Quaternion.identity) as GameObject).gameObject.transform.SetParent(platforms_holder.transform);
+
+		game_over_image.SetActive (false);
+
+		GameObject[] p = GameObject.FindGameObjectsWithTag ("Player");
+		if (p.Length ==  0) {
+			Instantiate (player_settings.player, new Vector3 (0.0f, 5.0f, 0.0f), Quaternion.identity);
+		} else {
+			Destroy (p [0]);
+			Instantiate (player_settings.player, new Vector3 (0.0f, 5.0f, 0.0f), Quaternion.identity);
+		}
+	}
+
+	public void GetReference(){
+		platforms_holder = GameObject.Find ("Platforms").transform;
+		foreach (Transform child in platforms_holder.transform) {
+			GameObject.Destroy (child.gameObject);
+		}
+
+		fallings_holder = GameObject.Find ("Fallings").transform;
+		foreach (Transform child in fallings_holder.transform) {
+			GameObject.Destroy (child.gameObject);
+		}
+
+		erruption = GameObject.Find ("Erruption").GetComponent<ParticleSystem> ();
+		ashes = GameObject.Find ("Ashes").GetComponent<ParticleSystem> ();
+		clounds = GameObject.Find ("Clounds").GetComponent<ParticleSystem> ();
+
+		blockImage = GameObject.Find ("Block Image");
+		game_over_image = GameObject.Find ("Game Over Image");
+
+		message_text = GameObject.Find ("Message Text").GetComponent<Text>();
+	}
+
+	IEnumerator SpawnPlatforms() {
+
+		yield return new WaitForSeconds (platform_settings.spawn_start_wait);
+
+		while(true) {
+			Vector2 spawnPosition = new Vector2(platform_settings.spawn_x, platform_settings.spawn_y);
+
+			GameObject toInstantiate = platform_settings.platforms[Random.Range(0, platform_settings.platforms.Length)];
+			GameObject instance = Instantiate (toInstantiate, spawnPosition, Quaternion.identity) as GameObject;
+			instance.transform.SetParent (platforms_holder);
+
+			yield return new WaitForSeconds (Random.Range(platform_settings.spawn_min_wait, platform_settings.spawn_max_wait));
+
+			if (!CanUpdate()) {
+				break;
+			}
+		}
+	}
+
+	IEnumerator SpawnFallings() {
+		yield return new WaitForSeconds (falling_settings.spawn_start_wait);
+
+		while (true) {
+			for (int i = 0; i < falling_settings.max_spawn; i++) {
+				Vector2 spawnPosition = new Vector2(Random.Range(-falling_settings.spawn_x, falling_settings.spawn_x), falling_settings.spawn_y);
+				GameObject toInstantiate = falling_settings.fallings[Random.Range(0, falling_settings.fallings.Length)];
+
+				GameObject instance = Instantiate (toInstantiate, spawnPosition, Quaternion.identity) as GameObject;
+				instance.transform.SetParent (fallings_holder);
+
+				yield return new WaitForSeconds (falling_settings.spawn_rate);
+
+				if (!CanUpdate()) {
+					break;
+				}
+			}
+
+			yield return new WaitForSeconds (falling_settings.spawn_wait);
+
+			if (!CanUpdate()) {
+				break;
+			}
+		}
+	}
+
+	void Update(){
+		if (play_game) {
+			bool b;
+			//#if UNITY_STANDALONE || UNITY_WEBPLAYER
+				b = Input.GetKeyDown (KeyCode.R);
+			//#else
+			//	b = Input.touchCount > 0 ? true : false;
+			//#endif
+
+			if(!game_start && b){
+				message_text.text = "";
+				game_start = true;
+
+				erruption.Play ();
+				ashes.Play ();
+				clounds.Play ();
+
+				StartCoroutine (SpawnPlatforms ());
+				StartCoroutine (SpawnFallings ());
+
+				blockImage.SetActive (false);
+				ShowIntroductionUI (false);
+			}
+
+			if(game_over && b){
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+		}
+	}
+
+
+	public bool CanUpdate(){
+		return game_start && !game_over;
+	}
+
+	public void GameOver() {
+		game_over = true;
+		message_text.text = "\n\n\n\nYou are incinerated.\n" + 
+			"You grow until " + score;
+
+		blockImage.SetActive (true);
+		game_over_image.SetActive (true);
+	}
+
+	public void PlayGame(bool b){
+		play_game = b;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void ShowMainMenuUI(bool b){
+		GameObject obj = GameObject.Find ("MainMenu UI");
+
+		foreach (Transform child in obj.GetComponentsInChildren<Transform>(true)) {
+			child.gameObject.SetActive (b);
+		}
+		obj.SetActive (true);
+	}
+
+	public void ShowInstructionUI(bool b){
+		GameObject obj = GameObject.Find ("Instruction UI");
+
+		foreach (Transform child in obj.GetComponentsInChildren<Transform>(true)) {
+			child.gameObject.SetActive (b);
+		}
+		obj.SetActive (true);
+	}
+		
+	public void ShowIntroductionUI(bool b){
+		GameObject obj = GameObject.Find ("Introduction UI");
+
+		foreach (Transform child in obj.GetComponentsInChildren<Transform>(true)) {
+			child.gameObject.SetActive (b);
+		}
+		obj.SetActive (true);
+	}
+
+	public void ShowInGameUI(bool b){
+		GameObject obj = GameObject.Find ("InGame UI");
+
+		foreach (Transform child in obj.GetComponentsInChildren<Transform>(true)) {
+			child.gameObject.SetActive (b);
+		}
+		obj.SetActive (true);
+	}
+		
+	public void ShowGameMap(bool b){
+		GameObject obj = GameObject.Find ("GameMap");
+
+		foreach (Transform child in obj.GetComponentsInChildren<Transform>(true)) {
+			child.gameObject.SetActive (b);
+		}
+		obj.SetActive (true);
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+
+	public void PlayerMoveRight(){
+		PlayerController pc = GameObject.Find ("Player").GetComponent<PlayerController> ();
+		pc.MoveRight ();
+	}
+
+	public void PlayerMoveLeft(){
+		PlayerController pc = GameObject.Find ("Player").GetComponent<PlayerController> ();
+		pc.MoveLeft ();
+	}
+
+	public void PlayerJump(){
+		PlayerController pc = GameObject.Find ("Player").GetComponent<PlayerController> ();
+		pc.Jump ();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private bool facingRight;
+	private bool jump;
+	private bool grounded;
+
+	[HideInInspector]
+	public int hp;
+
+	private Text hp_text;
+
+	private Rigidbody2D rb2d;
+
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Start(){
+		facingRight = true;
+		jump = false;
+		grounded = false;
+
+		hp_text = GameObject.Find ("HP Text").GetComponent<Text>();
+
+		hp = GameController.instance.player_settings.hp;
+
+		hp_text.text = "HP: " + hp;
+	}
+
+	void Update () {
+		if (GameController.instance.CanUpdate ()) {
+			if (Input.GetButtonDown ("Jump") && grounded) {
+				jump = true;
+			}
+
+			if (grounded) {
+				float speed = GameController.instance.platform_settings.scroll_speed;
+				transform.position = transform.position + Vector3.left * Time.deltaTime * speed;
+			}
+		} else if (hp <= 0) {
+			transform.position = transform.position + Vector3.down * Time.deltaTime;
+		}
+	}
+		
+	void FixedUpdate() {
+		if (GameController.instance.CanUpdate()) {
+
+			//#if UNITY_STANDALONE || UNITY_WEBPLAYER
+			float h = Input.GetAxis ("Horizontal");
+			float max_speed = GameController.instance.player_settings.max_speed;
+			if (h * rb2d.velocity.x < max_speed) {
+				float move_force = GameController.instance.player_settings.move_force;
+				rb2d.AddForce (Vector2.right * h * move_force);
+			}
+
+			if (Mathf.Abs (rb2d.velocity.x) > max_speed) {
+				rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * max_speed, rb2d.velocity.y);
+			}
+
+			if (h > 0 && !facingRight) {
+				Flip ();
+			} else if (h < 0 && facingRight) {
+				Flip ();
+			}
+			
+			if (jump) {
+				float jump_force = GameController.instance.player_settings.jump_force;
+				rb2d.AddForce (new Vector2 (0f, jump_force));
+				jump = false;
+				grounded = false;
+			}
+			//#endif
+		}
+	}
+
+	public void MoveRight(){
+		float max_speed = GameController.instance.player_settings.max_speed;
+
+		if (rb2d.velocity.x < max_speed) {
+			float move_force = GameController.instance.player_settings.move_force;
+			rb2d.AddForce (Vector2.right * move_force);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > max_speed) {
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * max_speed, rb2d.velocity.y);
+		}
+
+		if (!facingRight) {
+			Flip ();
+		}
+	}
+
+	public void MoveLeft(){
+		float max_speed = GameController.instance.player_settings.max_speed;
+
+		if (-1 * rb2d.velocity.x < max_speed) {
+			float move_force = GameController.instance.player_settings.move_force;
+			rb2d.AddForce (Vector2.left * move_force);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > max_speed) {
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * max_speed, rb2d.velocity.y);
+		}
+
+		if (facingRight) {
+			Flip ();
+		}
+	}
+
+	public void Jump(){
+		if (grounded) {
+			grounded = false;
+			float jump_force = GameController.instance.player_settings.jump_force;
+			rb2d.AddForce (new Vector2 (0f, jump_force));
+			grounded = false;
+		}
+	}
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.CompareTag("Lava")) {
+			grounded = true;
+		}
+		else if(coll.gameObject.CompareTag("Platform")) {
+			grounded = true;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll){
+		if(coll.gameObject.CompareTag("Lava")){
+			int damage = coll.gameObject.GetComponent<LavaController> ().damage;
+
+			UpdateHP (damage);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag("Falling")){
+			int damage = other.gameObject.GetComponent<FallingController> ().damage;
+			UpdateHP (damage);
+
+			Destroy (other.gameObject);
+		}
+	}
+
+	void UpdateHP(int n){
+		hp -= n;
+
+		if (hp <= 0) {
+			hp = 0;
+			GameController.instance.GameOver ();
+		} else if (hp > 100) {
+			float rate = GameController.instance.player_settings.scale_rate;
+			float scale = 1 + hp / 100 * rate;
+
+			transform.localScale = new Vector3 (scale, scale ,1);
+		}
+
+		if (hp > GameController.instance.score) {
+			GameController.instance.score = hp;
+		}
+
+		hp_text.text = "HP: " + hp;
+	}
+}
